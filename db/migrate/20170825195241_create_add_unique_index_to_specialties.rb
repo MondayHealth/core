e21b9b45bd0c2bf4b4b04809eac8d119f521972f
Benjamin Jackson <bhjackson@gmail.com>
2017-08-25 15:53:00 -0400

    Added unique index on name to specialties table.

---
+++ db/migrate/20170825195241_create_add_unique_index_to_specialties.rb
@@ -0,0 +1,5 @@
+class CreateAddUniqueIndexToSpecialties < ActiveRecord::Migration[5.1]
+  def change
+    add_index :specialties, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825195140) do
+ActiveRecord::Schema.define(version: 20170825195241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170825195140) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_specialties_on_name", unique: true
   end
 
 end
