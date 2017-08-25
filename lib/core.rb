456e24b00edef1d8d06600dce98f580949af177e
Benjamin Jackson <bhjackson@gmail.com>
2017-08-25 15:37:38 -0400

    Added missing requires for new models to main file.

--- lib/core.rb
+++ lib/core.rb
@@ -1,6 +1,9 @@
 require 'otr-activerecord'
 require 'pg'
 
+require 'models/doctor'
+require 'models/location'
 require 'models/plan'
 require 'models/provider'
 require 'models/provider_record'
+require 'models/specialty'
