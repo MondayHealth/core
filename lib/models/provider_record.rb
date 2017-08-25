79211942f2acb276ceb90e01da88f02bdc1d4740
Benjamin Jackson <bhjackson@gmail.com>
2017-08-25 14:14:41 -0400

    Added validations to ProviderRecord model that match the database validations so that clients can use ActiveRecord$valid? to check before importing records.

--- lib/models/provider_record.rb
+++ lib/models/provider_record.rb
@@ -1,3 +1,12 @@
 class ProviderRecord < ActiveRecord::Base
   belongs_to :provider
+
+  validates_presence_of :provider
+  validates_presence_of :accepted_plan_ids
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :license
+  validates_presence_of :address
+  validates_presence_of :phone
+  validates_presence_of :specialties
 end
