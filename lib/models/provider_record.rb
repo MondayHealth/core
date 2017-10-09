6dea739e7242b4c2dae5f7dc82e0d8d814d128e2
Benjamin Jackson <bhjackson@gmail.com>
2017-10-09 14:25:52 -0400

    Updated ProviderRecord model to no longer require a phone number, a payor or accepted plans when pulled for a directory, and tied it to the Directory model with a belongs_to association.

--- lib/models/provider_record.rb
+++ lib/models/provider_record.rb
@@ -1,13 +1,14 @@
 class ProviderRecord < ActiveRecord::Base
   belongs_to :payor
   belongs_to :provider
+  belongs_to :directory
 
-  validates_presence_of :payor
-  validates_presence_of :accepted_plan_ids
+  validates_presence_of :payor, unless: ->(pr) { pr.directory.present? }
+  validates_presence_of :accepted_plan_ids, unless: -> (pr) { pr.directory.present? }
+  validates_presence_of :directory, unless: ->(pr) { pr.payor.present? }
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :license
   validates_presence_of :address
-  validates_presence_of :phone
   validates_presence_of :specialties
 end
