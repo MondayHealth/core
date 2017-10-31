f3808064ded447af28d245f34bd58b983d0b60f9
Benjamin Jackson <bhjackson@gmail.com>
2017-10-31 16:19:25 -0400

    Tweaked validations of ProviderRecord model to avoid pinging the database with a lookup on each validation, which was slowing down the CSV loader considerably.

--- lib/models/provider_record.rb
+++ lib/models/provider_record.rb
@@ -3,9 +3,9 @@ class ProviderRecord < ActiveRecord::Base
   belongs_to :directory
   belongs_to :provider
 
-  validates_presence_of :payor, unless: ->(pr) { pr.directory.present? }
-  validates_presence_of :accepted_plan_ids, unless: -> (pr) { pr.directory.present? }
-  validates_presence_of :directory, unless: ->(pr) { pr.payor.present? }
+  validates_presence_of :payor, unless: ->(pr) { pr.directory_id.present? }
+  validates_presence_of :accepted_plan_ids, unless: -> (pr) { pr.directory_id.present? }
+  validates_presence_of :directory, unless: ->(pr) { pr.payor_id.present? }
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :license
