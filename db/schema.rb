67e64e489f20edff869d9e2e9b7bd627ae25ec93
Benjamin Jackson <bhjackson@gmail.com>
2017-11-10 10:57:16 -0500

    Added seed data for Empire and updated plans table to take an original_code (called "Product Code" on Empire's site) for use in crawlers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031211757) do
+ActiveRecord::Schema.define(version: 20171110155628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171031211757) do
     t.integer "record_limit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "original_code"
     t.index ["payor_id", "name"], name: "index_plans_on_payor_id_and_name", unique: true
   end
 
