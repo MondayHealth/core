1be4ebe8c6fc8e6be866df35246dcda16713fc62
Benjamin Jackson <bhjackson@gmail.com>
2017-11-21 15:56:12 -0500

    Removed dependent: destroy rule from provider_records association in the provider model, since the provider_record can exist without an attached provider and we don't want to implicitly delete canonical data.

--- lib/models/provider.rb
+++ lib/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ActiveRecord::Base
+  has_many :provider_records
   has_many :locations, dependent: :destroy
-  has_many :provider_records, dependent: :destroy
   has_and_belongs_to_many :specialties
 end
